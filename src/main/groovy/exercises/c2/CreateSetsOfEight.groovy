f852fb14740e7d4fa5795a2ec06a7dfe23534a23
Gracey <40322132@live.napier.ac.uk>
2019-02-22 11:57:17 +0000

    Partial Working TestCase

--- src/main/groovy/exercises/c2/CreateSetsOfEight.groovy
+++ src/main/groovy/exercises/c2/CreateSetsOfEight.groovy
@@ -5,18 +5,18 @@ import jcsp.lang.*
 class CreateSetsOfEight implements CSProcess{
 	
 	def ChannelInput inChannel
-
+	def ChannelOutput outChannel
+	def outList = []
 	void run(){
-		def outList = []
 		def v = inChannel.read()
 		while (v != -1){
+			outList = []
 			for ( i in 0 .. 7 ) {
 				// put v into outList and read next input
 				outList.add(v)
 				v = inChannel.read()
 			}
 			println " Eight Object is ${outList}"
-			outList = []
 		}
 		println "Finished"
 	}
--- src/main/groovy/exercises/c2/SetsOfEightTest.groovy
+++ src/main/groovy/exercises/c2/SetsOfEightTest.groovy
@@ -1,4 +1,23 @@
 package exercises.c2
 
-class SetsOfEightTest {
+import groovyJCSP.*
+import jcsp.lang.*
+
+class SetsOfEightTest extends GroovyTestCase {
+    void testMessage() {
+        One2OneChannel connect1 = Channel.createOne2One()
+        One2OneChannel connect2 = Channel.createOne2One()
+        One2OneChannel toTest = Channel.createOne2One()
+        def generateSetsOfThree = new GenerateSetsOfThree( outChannel: connect1.out() )
+        def listToStream = new ListToStream( inChannel: connect1.in(), outChannel: connect2.out() )
+        def setsOfEight = new CreateSetsOfEight( inChannel: connect2.in(), outChannel: toTest.out())
+        def processList = [generateSetsOfThree, listToStream, setsOfEight]
+
+        new PAR(processList).run()
+
+        def expected = [[1, 2 , 3, 4, 5, 6, 7, 8], [9, 10, 11, 12, 13, 14, 15, 16], [17, 18, 19, 20, 21, 22, 23, 24]]
+        def actual = CreateSetsOfEight.outList
+
+        assertTrue(actual in expected)
+    }
 }
